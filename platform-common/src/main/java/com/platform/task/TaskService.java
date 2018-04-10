f6e2d20cdc09365998212eb5e9e6106bd0720d09
zhancl <javazcl@aliyun.com>
2018-04-10 16:45:44 +0800

    定时任务

---
+++ platform-common/src/main/java/com/platform/task/TaskService.java
@@ -0,0 +1,17 @@
+package com.platform.task;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by ttui on 2018/1/11.
+ */
+@Component
+public class TaskService {
+
+    public void batchUpdateOrderCancelStatus() {
+
+        System.out.println("订单取消成功！");
+
+
+    }
+}
